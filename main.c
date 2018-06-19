df03455037f923bf754804fc1ab910c7af5c254d
Tristan Hilbert <tchilbert@live.com>
2018-06-19 09:52:50 -0700

    So far so good. Need to add test checks and better printing

--- main.c
+++ main.c
@@ -10,25 +10,29 @@
 #include "tree/binary_tree.h"
 
 int main(int argc, char * argv[]) {
-	FILE * text_input;
-	int i;
-	struct bin_tree a;
+   FILE * text_input;
+   int i;
+   struct bin_tree a;
 
-	if (!argv[1]) {
-		printf("Needed Arguments:\n\tText File \".txt\"\n");
-		return 1;
-	}
-	else {
+   if (!argv[1]) {
+      printf("Needed Arguments:\n\tText File \".txt\"\n");
+      return 1;
+   }
+   else {
 
-		init_tree(&a);
-		text_input = fopen(argv[1], "r");
-		while ((fscanf(text_input, "%i", &i)) != EOF) {
-			add_tree(&a, i);
-		}
-		printf("\n\n");
-		fclose(text_input);
-		print_tree(&a);
-		del_tree(&a);
-	}
-	return 0;
+      init_tree(&a);
+      text_input = fopen(argv[1], "r");
+      while ((fscanf(text_input, "%i", &i)) != EOF) {
+	 add_tree(&a, i);
+      }
+      printf("\n\n");
+      fclose(text_input);
+      print_tree(&a);
+      printf("\n");
+      remove_tree(&a, i);
+      print_tree(&a);
+      printf("The minimum Value is : %d\n\n", get_min(&a));
+      del_tree(&a);
+   }
+   return 0;
 }
